02177bf4cb770242281eee2964eff1bfc78a71fc
eh0n3y <93060317+eh0n3y@users.noreply.github.com>
2022-03-21 11:51:26 -0500

    Add files via upload

--- helloWorld.py
+++ helloWorld.py
@@ -1,3 +1,7 @@
 print ("Hello World")
 print ("Hello World, my name is <Emma> and I love Python!")
 print ("Hello World, my name is Emma and I love Python!")
+userName = input ("Hello! Please enter you name!")
+print ("Hello", userName, "nice to meet you")
+userName = input ("Hello! Emma!")
+print ("Hello" , Emma, "nice to meet you")
